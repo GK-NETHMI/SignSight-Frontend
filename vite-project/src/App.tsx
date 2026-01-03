aa18e14f6b08a7b6ef0801b135aecde0db682ff9
Hasandara Sandamini <hasandarasandamini164@gmail.com>
2026-01-03 09:36:42 +0530

    setup react web

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,2 +1,13 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Em
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Home Page</h1>} />
+        <Route path="/about" element={<h1>About Page</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
