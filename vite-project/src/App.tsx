496c4b801a5c26dfddd1eb82d5a386a04a48ecf6
Hasandara Sandamini <hasandarasandamini164@gmail.com>
2026-01-06 20:49:05 +0530

    checking errors

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,14 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Emotion_Landing from "./pages/Emotion_Landing";  
-
-
+import Emotion_landing from "./pages/Emotion_Landing";
+import Instructions from "./pages/Instructions";
+import EmotionFlow from "./pages/EmotionFlow";
+import Result from "./pages/Result";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/emotion_landing" element={<Emotion_Landing />} />
-        <Route path="/about" element={<h1>About Page</h1>} />
+        <Route path="/emotion_landing" element={<Emotion_landing />} />
+        <Route path="/instructions" element={<Instructions />} />
+        <Route path="/emotion" element={<EmotionFlow />} />
+        <Route path="/result" element={<Result />} />
       </Routes>
     </BrowserRouter>
   );
