49806c2572c3760dfd1362846bedc8589d6ddd9c
Kaveesha <nethmikavee40@gmail.com>
2026-01-06 19:15:58 +0530

    implement

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,14 +1,34 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Emotion_Landing from "./pages/Emotion_Landing";  
+import Emotion_landing from "./pages/hasadara/Emotion_Landing";
+import MentorLogin from "./pages/kaveesha/MentorLogin";
+import MentorSignUp from "./pages/kaveesha/MentorSignUp";
+import StudentLearningLanding from "./pages/kaveesha/StudentLanding";
+import QuizEngine from "./pages/kaveesha/learn/QuizEngine";
+import Results from "./pages/kaveesha/ResultPage";
+import LessonsHome from "./pages/kaveesha/lessons/LessonsHome";
 
-
+import LessonPlayer from "./pages/kaveesha/lessons/LessonsPlayer";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/emotion_landing" element={<Emotion_Landing />} />
-        <Route path="/about" element={<h1>About Page</h1>} />
+        <Route path="/emotion_landing" element={<Emotion_landing />} />
+
+        <Route path="/student/landing" element={<StudentLearningLanding />} />
+        <Route path="/mentor/login" element={<MentorLogin />} />
+        <Route path="/mentor/signup" element={<MentorSignUp />} />
+        <Route path="/results" element={<Results />} />
+
+        <Route path="/learn/:level" element={<QuizEngine />} />
+
+        <Route path="/lessons" element={<LessonsHome />} />
+
+        <Route path="/lessons" element={<LessonsHome />} />
+        <Route path="/lessons/:category/:item" element={<LessonPlayer />} />
+
+        {/* <Route path="/lessons/food/:food" element={<FoodPlayer />} />
+        <Route path="/lessons/numbers/:number" element={<NumberViewer />} /> */}
       </Routes>
     </BrowserRouter>
   );
