7a5c756915dbdbe449c63e66598b4ac5b7b8ac07
Kaveesha <nethmikavee40@gmail.com>
2026-05-03 14:35:52 +0530

    port changed

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:5050",
+        target: "http://127.0.0.1:5080",
         changeOrigin: true,
         secure: false,
       },
